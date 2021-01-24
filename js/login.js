53f05f0c041d073948194a4232e8b38885d30725
drew-gits <69527486+drew-gits@users.noreply.github.com>
2021-01-23 23:39:05 -0500

    Split login and signup pages
    split login.html into index.html and signup.html pages with the former index.html becoming main.html

--- js/login.js
+++ js/login.js
@@ -1,17 +1,5 @@
 $(document).ready(function () {
-    $("#signup").click(function () {
-        $("#first").fadeOut("fast", function () {
-            $("#second").fadeIn("fast");
-            document.title = 'Sign Up' + TITLE_STR;
-        });
-    });
-
-    $("#signin").click(function () {
-        $("#second").fadeOut("fast", function () {
-            $("#first").fadeIn("fast");
-            document.title = 'Login' + TITLE_STR;
-        });
-    });
+    document.title = 'Login' + TITLE_STR;
 
     $(function () {
         $("form[name='login']").validate({
@@ -71,107 +59,9 @@ $(document).ready(function () {
 
                     saveCookie();
 
-                    window.location.href = "index.html";
+                    window.location.href = "main.html";
                 } catch (err) {
                     $("#loginResult").html(err.message);
-                }
-            }
-        });
-    });
-
-    $(function () {
-        $("form[name='registration']").validate({
-            rules: {
-                firstname: {
-                    required: true,
-                },
-                lastname: {
-                    required: true,
-                },
-                username1: {
-                    required: true,
-                },
-                password1: {
-                    required: true,
-                    minlength: 5
-                },
-                passwordConfirm: {
-                    equalTo: "#password1"
-                }
-            },
-
-            messages: {
-                firstname: "Please enter your first Name",
-                lastname: "Please enter your last Name",
-                username1: "Please enter a valid Username",
-                password1: {
-                    required: "Please provide a password",
-                    minlength: "Your password must be at least 5 characters long"
-                },
-                passwordConfirm: {
-                    equalTo: "Your passwords do not match"
-                },
-            },
-
-            submitHandler: function (form) {
-
-                userID = 0;
-                firstName = $("#firstname").val();;
-                lastName = $("#lastname").val();;
-
-                let login = $("#username1").val();
-                let password = $("#password1").val();
-                //	var hash = md5( password );
-
-                $("#signupResult").html("");
-
-                //	var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
-                
-                userExists = false;
-                
-                /*
-                $.ajax({
-                    type: "POST",
-                    url: baseURL + "/CheckUser.php",
-                    data: JSON.stringify({ login: $("#username1").val() }),
-                    contentType: "application/json; charset=utf-8",
-                    dataType:"json",
-                    success: function(res)
-                    {
-                        //If username exists, set userExists to true
-                        userExists = res.exists;
-                    }
-                });
-                */
-
-                if (userExists) {
-                    $("#signupResult").html("Username is Already Taken");
-                    return;
-                }
-
-                let obj = {
-                    login: login,
-                    password: password,
-                    firstName: firstName,
-                    lastName: lastName
-                };
-                let jsonPayload = JSON.stringify(obj);
-
-                let url = baseURL + '/Signup.' + extension;
-
-                let xhr = new XMLHttpRequest();
-                xhr.open("POST", url, false);
-                xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-                try {
-                    xhr.send(jsonPayload);
-
-                    let jsonObject = JSON.parse(xhr.responseText);
-
-                    userID = jsonObject.id;
-
-                    window.location.href = "login.html";
-                } catch (err) {
-                    $("#signupResult").html(err.message);
                 }
             }
         });
