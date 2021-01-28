5cae1bd553a4661073117c476e9f6577c9eff4b1
CraigBaron <40808671+CraigBaron@users.noreply.github.com>
2021-01-27 22:01:25 -0500

    search funtion is now working correct.

--- js/search.js
+++ js/search.js
@@ -1,8 +1,24 @@
 //search should be called on click of the search bar button, it should take the input chars and the active user ID and send to backend
+function buildTable(data, numContacts){
+  table = document.getElementById('contactsTable')
+  table.innerHTML = ''
+  for (var i = 0; i < numContacts; i++){
+    var row = `<tr>
+            <td>${data[i].ID}</td>
+            <td>${data[i].FirstName}</td>
+            <td>${data[i].LastName}</td>
+            <td>${data[i].Phone}</td>
+            <td>${data[i].Email}</td>
+            <td><button type="button" class="btn" data-toggle="modal" data-target="#editContact"><span style="color: gray"class="fas fa-cog"></span></button><button type="button" class="btn" data-toggle="modal" data-target="#"><span style="color: tomato"class="fas fa-trash-alt"></span></button></td>
+          </tr>`
+    table.innerHTML += row
+  }
+}
   function searchFuntion()
   {
     var searchResult = document.getElementById("searchBar").value;
-
+    var numContacts = '';
+    let parse = '';
     let jsonPayload = JSON.stringify({
         userID: userID,
         search: searchResult,
@@ -19,16 +35,13 @@
               $("#contacts").html("Contact(s) List");
               let jsonObject = JSON.parse(xhr.responseText);
 
-              for (let i = 0; i < jsonObject.contacts.length; i++) {
-                  contactsList += "<li class=\"list-group-item\">" + JSON.stringify(jsonObject.contacts[i]) + "</li>";
-              }
-              $("#contactsList").html(contactsList);
+              numContacts = jsonObject.contacts.length;
+              buildTable(jsonObject.contacts, numContacts);
           }
       };
       xhr.send(jsonPayload);
     }catch (err) {
         $("#contactsList").html(err.message);
     }
-    buildTable(contactsList);
 
   }
