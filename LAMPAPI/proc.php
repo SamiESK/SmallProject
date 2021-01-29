6e0740f444c6552d7af7b887996024805e162bdb
Sami Eskirjeh <32425558+SamiESK@users.noreply.github.com>
2021-01-29 00:27:49 -0500

    Added php file
    New php file to verify if username already exists in the database

---
+++ LAMPAPI/proc.php
@@ -0,0 +1,13 @@
+<?php
+
+  require_once("config.php");
+  
+	$inData = getRequestInfo();
+	$id = 0;
+  $sql_u = "SELECT * FROM users WHERE ID='$id'";
+  $res_u = mysqli_query($conn, $sql_u);
+  
+  if (mysqli_num_rows($res_u) > 0) 
+    returnWithError( "Username already exists." );
+    
+?>
