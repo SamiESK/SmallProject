6d8273c8df771e6309efee3a52e9c2be16f5f45d
Sami Eskirjeh <32425558+SamiESK@users.noreply.github.com>
2021-01-29 07:32:24 -0500

    Update proc.php

--- LAMPAPI/proc.php
+++ LAMPAPI/proc.php
@@ -4,10 +4,35 @@
   
 	$inData = getRequestInfo();
 	$id = 0;
-  $sql_u = "SELECT * FROM users WHERE ID='$id'";
+  $sql_u = "SELECT * FROM User WHERE ID='$id'";
   $res_u = mysqli_query($conn, $sql_u);
   
   if (mysqli_num_rows($res_u) < 0) 
-    returnWithError( "Username already exists." );
+  {
+    $sql = "INSERT INTO User (Login, Password, FirstName, LastName) VALUES (?,?,?,?)";
+    $stmt= $conn->prepare($sql);
     
+    $stmt->bind_param("ssss", 
+        $inData["login"], 
+        $inData["password"], 
+        $inData["firstName"], 
+        $inData["lastName"]);
+    
+    $stmt->execute();
+
+    $sql = "SELECT ID FROM User where Login='" . $inData["login"] . "' and Password='" . $inData["password"] . "'";
+
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0)
+    {
+        $row = $result->fetch_assoc();
+        $id = $row["ID"];
+        
+        returnWithInfo($inData["firstName"], $inData["lastName"], $id);
+    }
+    else 
+    {
+      returnWithError( "Username already exists." );
+    }
+  }    
 ?>
