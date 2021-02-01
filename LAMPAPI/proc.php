d2281acf14f120eb93f57f56e4e13bb8f3c7a87f
Sami Eskirjeh <32425558+SamiESK@users.noreply.github.com>
2021-02-01 18:41:13 -0500

    Fixed proc .php to check if username is valid

--- LAMPAPI/proc.php
+++ LAMPAPI/proc.php
@@ -1,38 +1,42 @@
 <?php
 
   require_once("config.php");
-  
+  $msg  = "";
 	$inData = getRequestInfo();
 	$id = 0;
-  $sql_u = "SELECT * FROM User WHERE ID='$id'";
-  $res_u = mysqli_query($conn, $sql_u);
+  $sql_u = "($conn,SELECT * FROM User WHERE ID=" . $inData["id"] . ")";
+  $res_u = mysqli_num_rows($sql_u);
   
-  if (mysqli_num_rows($res_u) < 0) 
+  if ($res_u > 0) 
   {
-    $sql = "INSERT INTO User (Login, Password, FirstName, LastName) VALUES (?,?,?,?)";
-    $stmt= $conn->prepare($sql);
-    
-    $stmt->bind_param("ssss", 
-        $inData["login"], 
-        $inData["password"], 
-        $inData["firstName"], 
-        $inData["lastName"]);
-    
-    $stmt->execute();
-
-    $sql = "SELECT ID FROM User where Login='" . $inData["login"] . "' and Password='" . $inData["password"] . "'";
-
-    $result = $conn->query($sql);
-    elseif ($result->num_rows > 0)
-    {
-        $row = $result->fetch_assoc();
-        $id = $row["ID"];
-        
-        returnWithInfo($inData["firstName"], $inData["lastName"], $id);
-    }
+    $msg = "Username Already Taken.";
     else 
     {
-      returnWithError( "Username already exists." );
+      $sql = "INSERT INTO User (Login, Password, FirstName, LastName) VALUES (?,?,?,?)";
+      $stmt= $conn->prepare($sql);
+      
+      $stmt->bind_param("ssss", 
+          $inData["login"], 
+          $inData["password"], 
+          $inData["firstName"], 
+          $inData["lastName"]);
+      
+      $stmt->execute();
+
+      $sql = "SELECT ID FROM User where Login='" . $inData["login"] . "' and Password='" . $inData["password"] . "'";
+
+      $result = $conn->query($sql);
+      if ($result->num_rows > 0)
+      {
+          $row = $result->fetch_assoc();
+          $id = $row["ID"];
+          
+          returnWithInfo($inData["firstName"], $inData["lastName"], $id);
+      }
+      else
+      {
+          returnWithError("Error fetching from database");
+      }
     }
   }    
 ?>
