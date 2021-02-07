1fe03bf5b5ce4a4b1d92fc2a3d146de4f21ba4de
drew-gits <69527486+drew-gits@users.noreply.github.com>
2021-02-07 02:59:49 -0500

    Update config.js to prevent cors

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const TITLE_STR = ' - Spira Contacts'
 
-var baseURL = 'http://' + 'www.cop4331-theboys.xyz/LAMPAPI';
+var baseURL = 'http://' + window.location.hostname + '/LAMPAPI';
 var extension = 'php';
 var userID = 0;
 var firstName = "";
