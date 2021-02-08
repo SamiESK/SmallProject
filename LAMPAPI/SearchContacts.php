e99c8cfd95df2b85bf9699ca0501d904d1edfb19
drew-gits <69527486+drew-gits@users.noreply.github.com>
2021-02-08 01:11:51 -0500

    Improve results of search API.

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -8,6 +8,8 @@
     $sql = "SELECT * FROM Contacts WHERE UserID='" . $inData["userID"] . "'";
     $sql .= " AND (FirstName LIKE '%" . $search . "%'";
     $sql .= " OR LastName LIKE '%" . $search . "%'";
+    $sql .= " OR concat(FirstName, ' ', LastName) LIKE '%" . $search . "%'";
+    $sql .= " OR concat(LastName, ' ', FirstName) LIKE '%" . $search . "%'";
     $sql .= " OR Phone LIKE '%" . $search . "%'";
     $sql .= " OR DateCreated LIKE '%" . $search . "%'";
     $sql .= " OR Email LIKE '%" . $search . "%')";
